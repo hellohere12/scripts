9d63dea7337523b5651045ce25bda54f9eabdabc
wqrey <66238848+wqrey@users.noreply.github.com>
2022-01-30 06:43:24 +0000

    Update arsenalfarm.lua

--- arsenalfarm.lua
+++ arsenalfarm.lua
@@ -1,40 +1,44 @@
-   getgenv().AutoFarm = true 
-   local serverhop_Speed = 45
+if game.PlaceId ~= 286090429 then
+    return
+end
+getgenv().Farm = true 
+local server = 45
 spawn(function()
 while AutoFarm do task.wait()
-    pcall(function()
+ pcall(function()
 local args = {
-    [1] = "TBC"
+ [1] = "TBC"
+ [2] = "TRC"
 }
 
 game:GetService("ReplicatedStorage").Events.JoinTeam:FireServer(unpack(args))
 end) end end)
 
 spawn(function()
-while AutoFarm do task.wait(0.1)
+while Farm do task.wait(0.1)
 pcall(function()
 function getClosest()
-           local closestdistance = 210
-           local ClosetPlayer = nil
-           for i,v in pairs(game.Players:GetChildren()) do
-               if v ~= game.Players.LocalPlayer and v.Team ~= game.Players.LocalPlayer.Team then
-                   local distance = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).magnitude
-                   if distance < closestdistance then
-                       closestdistance = distance
-                       ClosetPlayer = v
-                   end
-               end
-           end
-           return ClosetPlayer
-           end 
-      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = getClosest().Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 5)
+        local closestdistance = 210
+        local ClosetPlayer = nil
+        for i,v in pairs(game.Players:GetChildren()) do
+            if v ~= game.Players.LocalPlayer and v.Team ~= game.Players.LocalPlayer.Team then
+                local distance = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).magnitude
+                if distance < closestdistance then
+                    closestdistance = distance
+                    ClosetPlayer = v
+                end
+            end
+        end
+        return ClosetPlayer
+        end 
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = getClosest().Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 5)
 end) end end)
 spawn(function()
-while AutoFarm do task.wait(0.2)
+while Farm do task.wait(0.2)
 pcall(function()
- for i,v in pairs(game.Players:GetPlayers()) do
-       if v.Name == game.Players.LocalPlayer.Name then
-           else
+for i,v in pairs(game.Players:GetPlayers()) do
+    if v.Name == game.Players.LocalPlayer.Name then
+        else
 
 local ohNumber1 = 80
 local ohInstance2 = workspace[v.Name].Hitbox
@@ -43,14 +47,20 @@ game:GetService("ReplicatedStorage").Events.FallDamage:FireServer(ohNumber1, ohI
 end end end) end end)
 
 spawn(function()
-while AutoFarm do task.wait(serverhop_Speed)
+while Farm do task.wait(hop)
 pcall(function()
-    for i = 1,9 do 
- local HttpService, TPService = game:GetService("HttpService"), game:GetService("TeleportService")
-        local ServersToTP = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100"))
-        for _, s in pairs(ServersToTP.data) do
-          if s.playing ~= s.maxPlayers then
-              TPService:TeleportToPlaceInstance(game.PlaceId, s.id)
-              wait()
-              rconsoleprint("\n\Farming to whatever.\n")
-         end end end end) end end)
+ for i = 1,9 do 
+local HttpService, TPService = game:GetService("HttpService"), game:GetService("TeleportService")
+     local ServersToTP = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100"))
+     for _, s in pairs(ServersToTP.data) do
+       if s.playing ~= s.maxPlayers then
+           TPService:TeleportToPlaceInstance(game.PlaceId, s.id)
+           wait()
+           local CoreGui = game:GetService("StarterGui")
+
+           CoreGui:SetCore("SendNotification", {
+            Title = "Farm";
+            Text = "Changing servers...";
+            Duration = 6;
+           })
+      end end end end) end end)
